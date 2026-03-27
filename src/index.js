394e9a343a872f934380a45f7d5f1ab55dbe2c2a
V-Narasimha <narasimhav2002@gmail.com>
2026-03-27 19:45:49 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client"
 import "./style.css"
-import App from "./App"
+import App from "./app.js"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<App></App>)
